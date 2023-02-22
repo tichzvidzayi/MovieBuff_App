bb464535cf9cd468fdc305d8fc5be2badefe3394
Tich Zvidzayi <tzvidzie@gmail.com>
2023-02-22 07:58:37 +0200

    add_api

--- movie_buffer_app/src/App.js
+++ movie_buffer_app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import MovCard from './Components/MovCard'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const apiURL = `http://www.omdbapi.com/?i=tt3896198&apikey=7f07872`;
+ const  getMovies = async ()=>{
+  try{
+    const data = await axios.get(apiURL);
+    console.log(data);
+  }
+  catch(err){
+     console.log(err + "error");
+  }
+
+
+ }
+ 
+ useEffect(()=>{
+  getMovies();
+
+ }, [])
+ 
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     </div>
   );
 }
 
